8102edae11d0e39e0f3cc442db49994a4d365d72
nuex <nx@nu-ex.com>
2009-01-28 11:40:28 -0500

    refactored poll_response_controller a bit

--- app/controllers/poll_response_controller.rb
+++ app/controllers/poll_response_controller.rb
@@ -7,16 +7,20 @@ class PollResponseController < ApplicationController
     @page.request, @page.response = request, response
 
     poll = Poll.find(params[:poll_id])
-    unless session[:submitted_polls]
-      session[:submitted_polls] = []
-      unless session[:submitted_polls].include?(poll.id)
-        ResponseCache.instance.expire_response(@page.url)
-        session[:submitted_polls] << poll.id
-        poll_response = Option.find(params[:response_id])
-        poll.submit_response(poll_response)
-      end
+    session[:submitted_polls] = [] unless session[:submitted_polls]
+    unless session[:submitted_polls].include?(poll.id)
+      ResponseCache.instance.expire_response(@page.url)
+
+      session[:submitted_polls] << poll.id
+
+      poll_response = Option.find(params[:response_id])
+      poll.submit_response(poll_response)
     end
+
+    # assign the session to the current page so we can
+    # pass it off to our radius tags
     @page.submitted_polls = session[:submitted_polls]
+
     redirect_to @page.url
   end
 
