e9829aa7e9e4a68abf4795b03f4a7fe3d7e5d9d9
David Cato <git@crunchyfrog.net>
2009-05-28 17:54:40 -0700

    Inherit from Admin::ResourceController

--- app/controllers/admin/polls_controller.rb
+++ app/controllers/admin/polls_controller.rb
@@ -1,42 +1,19 @@
-class Admin::PollsController < ApplicationController
-  def index
-    @polls = Poll.find(:all)
-  end
-
-  def new
-    @poll = Poll.new
-  end
-
-  def create
-    @poll = Poll.create(params[:poll])
-    flash[:notice] = "Poll '#{@poll.title}' Created"
-    redirect_to :action => :index
-  end
-
-  def edit
-    @poll = Poll.find(params[:id])
-  end
-
-  def update
-    @poll = Poll.find(params[:id])
-    if @poll.update_attributes(params[:poll])
-      flash[:notice] = "Successfully updated '#{@poll.title}'."
-      redirect_to :action => :index
-    else
-      render_action 'edit'
-    end
-  end
-
-  def destroy
-    Poll.destroy(params[:id])
-    flash[:notice] = "Poll destroyed"
-    redirect_to :action => :index
-  end
+class Admin::PollsController < Admin::ResourceController
+  model_class Poll
 
   def clear_responses
-    @poll = Poll.find(params[:id])
-    @poll.clear_responses
-    flash[:notice] = "Poll '#{@poll.title}' responses cleared."
+    if @poll = Poll.find(params[:id])
+      @poll.clear_responses
+      flash[:notice] = "Poll '#{@poll.title}' responses cleared."
+    end
     redirect_to :action => :index
   end
+
+  protected
+
+    def load_models
+      # Order polls by descending date, then alphabetically by title
+      self.models = model_class.all(:order => "COALESCE(start_date, '1900-01-01') DESC, title")
+    end
+
 end
