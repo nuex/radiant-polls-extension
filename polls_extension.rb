f8f6436998e82b2cc2564ecb44f1fe2c92ed81d4
nx <nx@nu-ex.com>
2010-01-21 09:47:44 -0500

    added <r:poll:options:each:if_first/> and <r:poll:options:each:if_last/>

--- polls_extension.rb
+++ polls_extension.rb
@@ -2,14 +2,14 @@ class PollsExtension < Radiant::Extension
   version '0.3.1'
   description 'Radiant gets polls.'
   url 'http://github.com/nuex/radiant-polls-extension'
-  
+
   define_routes do |map|
     map.namespace :admin, :member => { :clear_responses => :post } do |admin|
       admin.resources :polls
     end
     map.resources :poll_response, :only => [ :index, :create ], :path_prefix => '/pages/:page_id', :controller => 'poll_response'
   end
-  
+
   def activate
     require_dependency 'application_controller'
     admin.tabs.add 'Polls', '/admin/polls', :after => 'Layouts', :visibility => [:all]
@@ -22,9 +22,9 @@ class PollsExtension < Radiant::Extension
     Page.send :include, PollPageExtensions
     Page.send :include, ActionView::Helpers::TagHelper  # Required for pagination
   end
-  
+
   def deactivate
     # Not used
   end
-  
+
 end
